00fd0a0f592e989859153d17f42a7b24692baeef
Zev <eb01034949r@gmail.com>
2024-12-29 18:25:44 -0500

    add test

--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -1,3 +1,16 @@
+# test_views.py
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+
+    def test_home_view(self):
+        # 使用 Django 測試 Client 模擬請求
+        response = self.client.get(reverse('hello_world'))  # 假設路由名稱是 'home'
+
+        # 檢查 HTTP 狀態碼
+        self.assertEqual(response.status_code, 200)
+
+        # 檢查返回的內容
+        self.assertContains(response, "Hello, World!")  # 檢查是否包含這個字串
--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('hello/', views.hello_world, name='hello_world'),
 ]
